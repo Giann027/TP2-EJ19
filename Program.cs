5c7f2e8aeb164eb88d91cdfe2c23c1cc95c744dd
ricot <ricot@DESKTOP-FTHKA14>
2021-06-07 22:01:41 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TP2_EJ19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Buenasss");
+            Console.Title = "Superficie y Perimetro de un Rectángulo + exigencia lado > 0 ";
+
+            float Lado, Superficie, Perimetro;
+
+            Console.Write("Ingrese el lado mayor del rectángulo: ");
+            Lado = Convert.ToSingle(Console.ReadLine());
+
+            Superficie = Lado * Lado;
+
+            Perimetro = 4 * Lado;
+
+            if (Lado >= 0)
+                Console.WriteLine($"Para esas dimensiones, tendremos una superficie de {Superficie} mtrs^2 y un Perímetro de {Perimetro} mtrs ");
+            else
+                Console.WriteLine($"Error, por favor ingrese un valor mayor que cero ");
+        }
+    }
+}
